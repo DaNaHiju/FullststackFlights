3e1ebfa929c9dff0f0f76bd2680c801aa2950aa0
DaNaHiju <100130530+DaNaHiju@users.noreply.github.com>
2023-12-20 11:44:55 +0200

    Update authSlice.js

--- Front/src/redux/features/auth/authSlice.js
+++ Front/src/redux/features/auth/authSlice.js
@@ -1,50 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-const { access, refresh, user, permissions } = localStorage.getItem("auth") ? JSON.parse(localStorage.getItem("auth")) : {}
+// Retrieving user credentials from local storage
+const { access, refresh, user, permissions } = localStorage.getItem("auth") ? JSON.parse(localStorage.getItem("auth")) : {};
 const initialState = {
     access: access,
     refresh: refresh,
     user: user,
     permissions: permissions
-}
+};
+
+// Redux Slice for Authentication
 const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
+        // Action to set user credentials in the Redux store
         setCredentials: (state, action) => {
+            const { access, refresh, user, permissions } = action.payload;
+            state.access = access;
+            state.refresh = refresh;
+            state.user = user;
+            state.permissions = permissions;
 
-            const { access, refresh, user, permissions } = action.payload
-            state.access = access
-            state.refresh = refresh
-            state.user = user
-            state.permissions = permissions
-
-            localStorage.setItem("auth", JSON.stringify(action.payload))
+            // Saving credentials to local storage for persistent user sessions
+            localStorage.setItem("auth", JSON.stringify(action.payload));
         },
+        // Action to log out the user and clear credentials from the Redux store and local storage
         logOut: (state, action) => {
-            // state.user = null
-            state.access = null
-            state.refresh = null
-            state.user = null
-            state.permissions = null
-            localStorage.removeItem("auth")
+            state.access = null;
+            state.refresh = null;
+            state.user = null;
+            state.permissions = null;
+            localStorage.removeItem("auth");
         },
+        // Additional action for setting permissions if needed in the future
         // setPermissions: (state, action) => {
-            
-        //     state.access = null
-        //     state.refresh = null
-        //     state.user = null
-        //     state.permissions = null
-        //     localStorage.removeItem("auth")
-
+        //     // ... (see your code)
         // },
-
     }
-})
+});
 
+// Exporting Redux actions and reducer
+export const { setCredentials, logOut } = authSlice.actions;
+export default authSlice.reducer;
 
-export const { setCredentials, logOut } = authSlice.actions
-export default authSlice.reducer
-export const selectCurrentUser = (state) => state.auth.user
-export const selectCurrentToken = (state) => state.auth.access
+// Selectors for accessing current user and token from the Redux store
+export const selectCurrentUser = (state) => state.auth.user;
+export const selectCurrentToken = (state) => state.auth.access;
